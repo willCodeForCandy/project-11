dd7ac95fb3eb3089adf5b1fd400bf6861a2f0eda
Viru <viraschia@gmail.com>
2024-06-03 12:07:16 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+      <header>Header</header>
+      <main>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, id
+          dolor, tempora repellendus hic neque illo, porro rerum eos quam ut
+          recusandae et voluptatem culpa velit debitis veniam nihil iusto?
+        </p>
+      </main>
+      <aside>Aside</aside>
+      <footer>Footer</footer>
+    </>
+  );
+};
+
+export default App;
